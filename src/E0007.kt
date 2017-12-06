13536412540d22f61af28e6cf04a395cd80ee31f
JW de Roos <trueskimmer@gmail.com>
2017-12-06 20:38:10 +0100

    Number 7

---
+++ src/E0007.kt
@@ -0,0 +1,4 @@
+fun main(args: Array<String>) {
+    val primes : Primes = Primes()
+    println(primes.getPrime(10001))
+}
--- src/Primes.kt
+++ src/Primes.kt
@@ -1,21 +1,39 @@
 class Primes {
     fun generatePrimes(maxValue: Int) : List<Int> {
-        val numbers: MutableList<Int> = mutableListOf<Int>();
+        val numbers: MutableList<Int> = mutableListOf<Int>()
 
         for (i in 2..maxValue) {
             if (this.isPrime(i)) {
-                numbers.add(i);
+                numbers.add(i)
             }
         }
-        return numbers;
+        return numbers
+    }
+
+    fun generateFirstNPrimes(primeCount : Int) : List<Int> {
+        val primes: MutableList<Int> = mutableListOf<Int>()
+        var current : Int = 2;
+
+        while (primes.count() < primeCount) {
+            if (isPrime(current)) {
+                primes.add(current);
+            }
+            current++;
+        }
+
+        return primes
+    }
+
+    fun getPrime(index : Int) : Int {
+        return generateFirstNPrimes(index).last();
     }
 
     fun isPrime(number: Int) : Boolean {
         for (i in 2..number / 2) {
             if (number.rem(i) == 0) {
-                return false;
+                return false
             }
         }
-        return true;
+        return true
     }
 }
